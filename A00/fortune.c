2f14d9f36d70e220834905c5b2bc9585e5ba6ef0
Trinity Rae Kleckner <tkleckner@goldengate.cs.brynmawr.edu>
2022-01-20 19:57:48 +0000

    assignment 0 excercise

---
+++ A00/fortune.c
@@ -0,0 +1,13 @@
+/***************************************************
+ * fortune.c copied from hello.c
+ * Author: Aline Normoyle and Trinity Kleckner
+ * Date: 1/20/2022
+ * Print a fortune
+ */
+
+#include <stdio.h>
+
+int main() {
+  printf("Success is not a destination, it is a journey.\n");
+  return 0;
+}
