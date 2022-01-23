56f73fda327df15cdc10878486f5b75d4ee16c5e
Trinity <tkleckner@haverford.edu>
2022-01-23 12:23:48 -0500

    pass and snackbar

--- A01/password.c
+++ A01/password.c
@@ -1,3 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+char *makePass(char inp[100]) {
+  for(int i=0; inp[i]!='\0'; i++){
+    if(inp[i] == 'e'){
+      inp[i] = '3';
+    } else if(inp[i] == 'l'){
+      inp[i] = '1';
+    } else if(inp[i] == 'a'){
+      inp[i] = '@';
+    } 
+  }
+  return inp;
+}
+
 int main() {
+  char inp[100];
+  printf("Enter a word: ");
+  scanf("%s", inp);
+
+  char *outp = makePass(inp);
+  printf("%s\n",inp);
   return 0;
 }
+
--- A01/snackbar.c
+++ A01/snackbar.c
@@ -1,3 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+
+
+struct snack {
+  char name[50];
+  float cost;
+  int quant;
+};
+
+void printItems(struct snack arr[]);
+
 int main() {
+  struct snack snk1, snk2, snk3;
+
+  strcpy(snk1.name, "Ice Cream");
+  snk1.cost = 7.50;
+  snk1.quant = 10;
+
+  strcpy(snk2.name, "Reeses");
+  snk2.cost = 1.75;
+  snk2.quant = 7;
+
+  strcpy(snk3.name, "Pretzels");
+  snk3.cost = 5.25;
+  snk3.quant = 3;
+
+  struct snack snkArray[3] = {snk1, snk2, snk3};
+  float money;
+  int selection;
+
+  printf("Welcome to my snack bar.\n");
+  printf("\nHow much money do you have?  \n");
+  scanf("%g", money);
+  printItems(snkArray);
+  printf("\nWhat would you like to buy? [0,1,2]  \n");
+  scanf("%d", selection);
+  if(snkArray[selection].quant > 0){
+    printf("\nYou bought %s\n", snkArray[selection].name);
+    printf("You have $%g left", money-snkArray[selection].cost);
+  } else {
+    printf("Sorry, there are no more %s in stock",snkArray[selection].name); 
+  }
   return 0;
+}
+
+void printItems(struct snack arr[3]){
+  for(int i=0; i<3;i++){
+    printf("\n%d) %s \t cost: $%f \t quantity: %d", i, arr[i].name, arr[i].cost, arr[i].quant);
+  }
 }
