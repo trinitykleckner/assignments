43c49c2d1f1bb2b0f96d0dcfb52ca9dd8a7758c5
Trinity <tkleckner@haverford.edu>
2022-01-26 21:31:36 -0500

    the loop is working

--- A01/rpc.c
+++ A01/rpc.c
@@ -1,28 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
-int getWinner(char plyrChoice[9]);
+char* choiceToWord(int choice);
+char* printOutcome(char aiChoice[9], char plyrChoice[9]);
 
 int main() {
   int rounds;
   printf("Welcome to Rock, Paper, Scissors! \nHow many rounds do you want to play?  ");
   scanf("%d", &rounds);
-  printf("%d\n", rounds);
-
+  printf("%d", rounds);
+  
   int aiScore = 0;
   int plyrScore = 0;
 
-  for(int i=0; i<rounds; i++){
-    char choice[9];
-    printf("Which do you choose? rock, paper, or scissors?  ");
-    scanf("%s", choice);
+  int counter = rounds; //why do I need this?
+  for(int i=0; i < counter; i++){
+   // printf("in the loop i = %d rounds = %d",i,rounds);
+    char plyrChoice;
+    int aiChoice = 0;
+    char* aiChoiceWord;
+
+
+    printf("Which do you choose? rock, paper, or scissors? ");
+    scanf("%s", &plyrChoice);
+    aiChoiceWord = choiceToWord(aiChoice);
+    printf("AI chooses %s\n", aiChoiceWord);
+    printOutcome(aiChoiceWord, &plyrChoice); 
   }
   //srand(time(0));
   return 0;
 }
 
-int getWinner(char plyrChoice[9]){
-  srand(time(0));
-  int aiChoice;
+char *choiceToWord(int choice){
+  char* options[3] = {"rock", "paper", "scissors"};
+  for(int i = 0; i<3; i++){
+    //int cmp = strcmp(options[i],choice);
+    if(i == choice){
+      return options[i];
+    }
+  }
+  return "invalid choice";
+}
+
+char* printOutcome(char aiChoice[9], char plyrChoice[9]){
+  printf("in printOutcome");
+  return "end";
 }
