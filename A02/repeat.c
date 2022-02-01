fa6fe63f7db3ea5b1332ac8150a0d58c9a8a3edc
Trinity <tkleckner@haverford.edu>
2022-01-31 22:23:52 -0500

    started repeat

--- A02/repeat.c
+++ A02/repeat.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
 int main() {
+//need to use malloc and free
+  char inpString[32];
+  int inpInt;
+
+  printf("Enter a word: ");
+  scanf("%s", inpString);
+  printf("Enter a count: ");
+  scanf("%d", &inpInt);
+
+  printf("Your word is ");
+  for(int i=0; i<inpInt; i++){
+    printf("%s", inpString);
+  }
+
   return 0;
 }
