9881cc1dc683b8eb9b2e157848fd2c8bd003c751
Trinity <tkleckner@haverford.edu>
2022-02-21 21:49:21 -0500

    finished binarychar.c

--- A05/binarychar.c
+++ A05/binarychar.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
 int main() {
+  char input[8];
+  printf("Enter 8 bits: ");
+  scanf("%s", input);
+
+  int answer = 0;
+  for(int i=0; i<8; i++){
+    answer <<= 1;
+    if(input[i] == '1'){
+      answer += 1;
+    }
+  }
+  printf("Your character is: %c\n",answer);
   return 0;
 }
