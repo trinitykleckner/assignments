ea11e13c81912ee63be6a0ef394a26896cab47cd
Trinity <tkleckner@haverford.edu>
2022-02-24 15:12:14 -0500

    finished

--- J02/readvector.c
+++ J02/readvector.c
@@ -5,8 +5,25 @@
 // The size of the vector should be stored in the parameter size
 // The values should be returned in a flat float array
 float* readvector(const char* filename, int *size) {
-  // todo: implement me
-  return NULL;
+  FILE* infile = NULL;
+  infile = fopen(filename, "r");
+  if (infile == NULL) {
+      printf("Error open file\n");
+      exit(1);
+  }
+
+  char buf[33];
+  fgets(buf, 33, infile);
+  *size = atoi(buf);
+
+  float *vector = malloc(sizeof(float) * (*size));
+  int i = 0;
+  while (fgets(buf, 33, infile) != NULL) {
+    vector[i++] = (float) atof(buf);
+  }
+
+  fclose(infile);
+  return vector;
 }
 
 int main(int argc, char** argv) {
@@ -19,8 +36,11 @@ int main(int argc, char** argv) {
   float* vector = readvector(argv[1], &size);
 
   // todo: print vector values
+  for (int i = 0; i < size; i++) {
+    printf("%f\n", vector[i]);
+  }
 
   free(vector);
+  vector = NULL;
   return 0;
 }
-
