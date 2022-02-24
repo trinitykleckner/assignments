9ec77dfc4d35a6e640f84b61cd83374134871669
Trinity <tkleckner@haverford.edu>
2022-02-24 14:58:41 -0500

    this

--- J02/linkedlist.c
+++ J02/linkedlist.c
@@ -7,6 +7,18 @@ struct cake {
   struct cake* next;
 };
 
+void cheapestCake(struct cake* first){
+  struct cake* cheapest = first;
+  while(first != NULL){
+    printf("cake: %s ($%.2f)\n",first->flavor, first->cost);
+    if(first->cost < cheapest->cost){
+      cheapest = first;
+    }
+    first = first->next;
+  }
+  printf("The cheapest cake is %s\n",cheapest->flavor);
+}
+
 struct cake makeCake(const char* flavor, float cost) {
   struct cake newCake;
   strncpy(newCake.flavor, flavor, 16);
@@ -22,10 +34,13 @@ int main() {
   struct cake cake2 = makeCake("chocolate", 1.75);
   struct cake cake3 = makeCake("mocha", 3.50);
   // todo: define 4th cake
+  struct cake cake4 = makeCake("vanilla",4.00);
 
   cake1.next = &cake2;
   cake2.next = &cake3;
+  cake3.next = &cake4;
   // draw stack and heap here
 
   // todo: call cheapestCake
+  cheapestCake(&cake1);
 }
