b3d71510a521a276819c43bfae7b4c91cddeaf1d
Trinity <tkleckner@haverford.edu>
2022-03-02 22:37:24 -0500

    idk whats wrong

--- A06/decode.c
+++ A06/decode.c
@@ -1,7 +1,75 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "read_ppm.h"
 
+char* decode(char* encoded, int len);
+
 int main(int argc, char** argv) {
+  int rows;
+  int cols;
+
+  if(argc != 2){
+    printf("no file found");
+    return 1;
+  }
+
+  char* filename = argv[1];
+  struct ppm_pixel* image = read_ppm(filename,&rows,&cols);
+  printf("Reading %s with width %d and height %d\n", filename, rows, cols);
+  printf("Max number of characters in the image: %d\n", (rows*cols*3)/8);
+  char* message;
+  message = malloc(sizeof(char)*rows*cols*3);
+  if (message == NULL){
+    printf("malloc failed");
+    return 1;
+  }
+  //int i = 0;
+
+  //while(val.red != '\0'){
+  for(int i=0; i<rows*cols; i++){
+    printf("Red: %d\nGreen: %d\nBlue: %d\n",image[i].red,image[i].blue,image[i].green);
+
+    if(image[i].red%2 == 0){
+      message[i*3] = '0';
+    } else {
+      message[i*3] = '1';
+    }
+    if(image[i].green%2 == 0){
+      message[i*3+1] = '0';
+    } else {
+      message[i*3+1] = '1';
+    }
+    if(image[i].blue%2 == 0){
+      message[i*3+2] = '0';
+    } else {
+      message[i*3+2] = '1';
+    }
+    //i++;
+
+  }
+
+  int size = rows*cols*3;
+  while(size%8 != 0){
+    message[size] = '0';
+    size++;
+  }
+  message[size] = '\0';
+
+  printf("%s\n",message);
   return 0;
 }
 
+/*
+char* decode(char* encoded, int len){
+  char* decoded[len];
+  int counter = 0;
+  for(int i=0; i<len/8; i++){
+    char* thisChar[9];
+    for(int j=0; j<8; j++){
+      strcat(*thisChar, encoded[(counter*8)+1]);
+    }
+    char c = strtol(thisChar, 0, 2);
+    strcat(*decoded,c);
+    counter++;
+} */
--- A06/read_ppm.c
+++ A06/read_ppm.c
@@ -4,16 +4,71 @@
 #include "read_ppm.h"
 
 // TODO: Implement this function
-// Feel free to change the function signature if you prefer to implement an 
+// Feel free to change the function signature if you prefer to implement an
 // array of arrays
 struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
+  FILE *fp;
+  fp = fopen(filename,"rb");
+  if (fp == NULL) {
+      printf("Unable to open file\n");
+      return NULL;
+  }
 
-  return NULL;
+  char magicNumber[100];
+  char c[100];
+  int maxColor;
+
+  fgets(magicNumber, sizeof(char)*100, fp);
+  magicNumber[2] = '\0';
+  //check for comment
+  fgets(c, sizeof(char)*100, fp);
+  if(c[0] == '#'){
+    fgets(c, sizeof(char)*100, fp);
+  }
+  sscanf(c, " %d %d", w, h);
+  fscanf(fp, " %d ", &maxColor);
+
+  struct ppm_pixel* image;
+  image = malloc((*w)*(*h)*sizeof(struct ppm_pixel));
+  if (image == NULL){
+    printf("malloc failed");
+    return NULL;
+  }
+  fread(image, sizeof(struct ppm_pixel), (*h)*(*w), fp);
+
+  fclose(fp);
+  fp = NULL;
+  return image;
 }
 
 // TODO: Implement this function
-// Feel free to change the function signature if you prefer to implement an 
+// Feel free to change the function signature if you prefer to implement an
 // array of arrays
 extern void write_ppm(const char* filename, struct ppm_pixel* pxs, int w, int h) {
+  int j = 10;
+  for(int i=0; i<w*h; i++){
+    if(j%10==0){
+      pxs[i].red = (pxs[i].red ^ pxs[i].green) <<(rand()%1);
+      pxs[i].green = pxs[i].green && (rand()%10);
+      pxs[i].blue = pxs[i].blue & pxs[i].red;
+    } else {
+      pxs[i].red = 0;
+      pxs[i].green = (pxs[i].red ^ pxs[i].green) <<(rand()%1);
+      pxs[i].blue = pxs[i].green && (rand()%10);
+    }
+    if(i%10 == 0){
+      j++;
+    }
+  }
 
+
+  FILE* fp;
+  fp = fopen(filename,"wb");
+  char* toAdd = malloc(sizeof(char)*1000);
+  sprintf(toAdd, "P6\n%d %d\n255\n", w, h);
+  fwrite(toAdd, sizeof(int), 4, fp);
+  fwrite(pxs, sizeof(struct ppm_pixel), w*h, fp);
+
+  free(toAdd);
+  fclose(fp);
 }
