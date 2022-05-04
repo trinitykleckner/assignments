909b7b3e1a61bd17e80f1cb3808c6f1586fed3f1
Trinity <tkleckner@haverford.edu>
2022-05-04 15:52:22 -0400

    resubmit

--- A10/buddhabrot.c
+++ A10/buddhabrot.c
@@ -3,138 +3,130 @@
 #include <unistd.h>
 #include <assert.h>
 #include <time.h>
+#include <stdbool.h>
 #include <math.h>
+#include <string.h>
 #include <pthread.h>
 #include <sys/time.h>
+#include <sys/wait.h>
 #include "read_ppm.h"
 
 static unsigned long maxCount = 0;
 pthread_mutex_t mutex;
+pthread_barrier_t barrier;
 
-struct t_arg{
-  int *array;
-  struct ppm_pixel* image;
-  struct ppm_pixel* palette;
-  int* member;
-  int* counts;
-  int size;
-  int row;
-  int col;
-  float xmin;
-  float xmax;
-  float ymin;
-  float ymax;
-  int maxIterations;
+
+struct t_arg {
+    struct ppm_pixel * image;
+    struct ppm_pixel * palette;
+    bool * member;
+    int * count;
+    int rStart;
+    int cStart;
+    int rEnd;
+    int cEnd;
+    float xmin;
+    float xmax;
+    float ymin;
+    float ymax;
+    int maxIterations;
+    int size;
 };
 
-struct ppm_pixel* make_one(struct ppm_pixel* pxls, struct ppm_pixel* palette, int size, int row, int col, float xmin, float xmax, float ymin, float ymax, int maxIterations);
+void * startRoutine(void * inputData){
+    struct t_arg *this = (struct t_arg *) inputData;
 
+    //alg 1
+    for (int i = this->rStart; i < this->rEnd; i++) {
+        for (int j = this->cStart; j < this->cEnd; j++) {
+            float xfrac = i / (float) this->size;
+            float yfrac = j / (float) this->size;
+            float x0 = this->xmin + xfrac * (this->xmax - this->xmin);
+            float y0 = this->ymin + yfrac * (this->ymax - this->ymin);
 
-void *detMembership(void* args){
-  struct t_arg * this = (struct t_arg *) args;
-  int i, row, col;
+            float x = 0;
+            float y = 0;
+            int iter = 0;
 
-  printf("Thread %lu) sub-image blocks: cols (%d,%d) to rows (%d,%d)\n", pthread_self(), this->row, this->row+this->size/2, this->col, this->col+this->size/2);
+            while (iter < this->maxIterations && (x * x + y * y < 2 * 2)) {
+                float xtmp = x * x - y * y + x0;
+                y = 2 * x * y + y0;
+                x = xtmp;
 
-  for(int i=0; i<this->size/2; i++){
-    for(int j=0; j<this->size/2; j++){
-      float xfrac =  (float)(i+this->row)/(float) this->size;
-      float yfrac = (float)(j+this->col)/ (float) this->size;
-      float x0 = this->xmin + xfrac*(this->xmax-this->xmin);
-      float y0 = this->ymin + yfrac*(this->ymax-this->ymin);
-
-      float x=0;
-      float y=0;
-      int iter = 0;
-      while(iter < this->maxIterations && x*x + y*y < 2*2){
-        float xtmp = x*x - y*y + x0;
-        y = 2*x*y + y0;
-        x = xtmp;
-        iter ++;
-      }
-
-      if(iter < this->maxIterations){
-        this->member[(i+this->row)*this->size+(j+this->col)] = 0;
-      } else {
-        this->member[(i+this->row)*this->size+(j+this->col)] = 1;
-      }
-    }
-  }
-  return (void *)0;
-}
-
-void *compVisits(void* args){
-  struct t_arg * this = (struct t_arg *) args;
-  int i, row, col;
-  int allCount = 0;
-
-  for(int i=0; i<this->size/2; i++){
-    for(int j=0; j<this->size/2; j++){
-      if(this->member[(i+this->row)*this->size+(j+this->col)] == 1){
-        float xfrac =  (float)(i+this->row)/(float) this->size;
-        float yfrac = (float)(j+this->col)/ (float) this->size;
-        float x0 = this->xmin + xfrac*(this->xmax-this->xmin);
-        float y0 = this->ymin + yfrac*(this->ymax-this->ymin);
-
-        float x=0;
-        float y=0;
-        while(x*x + y*y < 2*2){
-          float xtmp = x*x - y*y + x0;
-          y = 2*x*y + y0;
-          x = xtmp;
-
-          float yrow = this->size*(y-this->ymin)/(this->ymax-this->ymin); //need to round this
-          float xcol = this->size*(x-this->xmin)/(this->xmax-this->xmin); //and this
-          if(yrow > 0 || yrow < this->size){
-            if(xcol > 0 || xcol < this->size){
-                this->counts[(j+this->row)+(i+this->col)*this->size] ++;
-                allCount ++;
-                //printf("%d\n",allCount);
+                iter++;
             }
-          }
+
+            if (iter < this->maxIterations) {
+                this->member[i * this->size + j] = false;
+            } else {
+                this->member[i * this->size + j] = true;
+            }
         }
-      }
     }
-  }
-  //printf("%ld\n",pthread_self());
-  //printf("\n\n\n %d\n\n",allCount);
 
-  pthread_mutex_lock(&mutex);
-  maxCount += allCount;
-  pthread_mutex_unlock(&mutex);
-  return (void *)0;
-}
+    //alg 2
+    for (int i = this->rStart; i < this->rEnd; i++) {
+        for (int j = this->cStart; j < this->cEnd; j++) {
+            if (this->member[i * this->size + j] == false) {
+                float xfrac = i / (float) this->size;
+                float yfrac = j / (float) this->size;
+                float x0 = this->xmin + xfrac * (this->xmax - this->xmin);
+                float y0 = this->ymin + yfrac * (this->ymax - this->ymin);
 
-void *compColors(void* args){
-  struct t_arg * this = (struct t_arg *) args;
-  int myid, i, row, col;
-  myid = *((int*) args);
-  float gamma = 0.681;
-  float factor = 1/gamma;
-  for(int i=0; i<this->size/2; i++){
-    for(int j=0; j<this->size/2; j++){
-      double value = 0;
-      //printf("%d\n",(j+this->row)+(i+this->col)*this->size);
-      if(this->counts[(i+this->row)*this->size+(j+this->col)] > 0){
-        printf("HERE");
-        value = log(this->counts[(i+this->row)*this->size+(j+this->col)])/log(maxCount);
-        value = pow(value,factor);
-      }
-      this->image[(i+this->row)*this->size+(j+this->col)].red = value*255;
-      this->image[(i+this->row)*this->size+(j+this->col)].blue = value*255;
-      this->image[(i+this->row)*this->size+(j+this->col)].green = value*255;
-      //printf("here- %f",value*255);
+                float x = 0;
+                float y = 0;
+                int iter = 0;
+
+                while (x*x + y*y < 2*2) {
+                    float xtmp = x*x - y*y + x0;
+                    y = 2*x*y + y0;
+                    x = xtmp;
+                    iter ++;
+
+                    int yrow = round(this->size * (y - this->ymin) / (this->ymax - this->ymin));
+                    int xcol = round(this->size * (x - this->xmin) / (this->xmax - this->xmin));
+
+                    if (yrow < 0 || yrow >= this->size) continue; // out of range
+                    if (xcol < 0 || xcol >= this->size) continue; // out of range
+
+                    pthread_mutex_lock(&mutex);
+                    this->count[yrow * this->size + xcol] += 1;
+                    pthread_mutex_unlock(&mutex);
+
+                    pthread_mutex_lock(&mutex);
+                    if (maxCount < this->count[yrow * this->size + xcol]) {
+                        maxCount = this->count[yrow * this->size + xcol];
+                    }
+                    pthread_mutex_unlock(&mutex);
+                }
+            }
+        }
     }
-  }
-  printf("Thread %lu) finished %d\n",pthread_self(), myid);
-  return (void *)0;
+    pthread_barrier_wait(&barrier);
+
+    //alg 3
+    double gamma = 0.681;
+    double factor = 1.0/gamma;
+    for (int i = this->rStart; i < this->rEnd; i++) {
+        for (int j = this->cStart; j < this->cEnd; j++) {
+            double value = 0;
+            if (this->count[i * this->size + j] > 0){
+                value = log(this->count[i * this->size + j])/log(maxCount);
+                value = pow(value, factor);
+            }
+            this->image[i * this->size + j].red = value * 255;
+            this->image[i * this->size + j].blue = value * 255;
+            this->image[i * this->size + j].green = value * 255;
+        }
+    }
+    return NULL;
 }
 
 
 int main(int argc, char* argv[]) {
   int size = 480;
-  float xmin = -2.0;
-  float xmax = 0.47;
+  float xmin = -2.5;
+  float xmax = 1.0;
   float ymin = -1.12;
   float ymax = 1.12;
   int maxIterations = 1000;
@@ -152,25 +144,24 @@ int main(int argc, char* argv[]) {
         "-b <ymin> -t <ymax> -p <numProcesses>\n", argv[0]); break;
     }
   }
+
   printf("Generating buddhabrot with size %dx%d\n", size, size);
   printf("  Num processes = %d\n", numProcesses);
-  printf("  X range = [%.4f,%.4f]\n", xmin, xmax);
-  printf("  Y range = [%.4f,%.4f]\n", ymin, ymax);
-  printf("Generating mandelbrot with size %dx%d\n", size, size);
   printf("  X range = [%.4f,%.4f]\n", xmin, xmax);
   printf("  Y range = [%.4f,%.4f]\n", ymin, ymax);
 
 
   srand(time(0));
+  double timer;
   struct timeval tstart, tend;
   gettimeofday(&tstart, NULL);
   pthread_t threads[4];
-  int ids[4];
-  struct t_arg *thread_args = malloc(4*sizeof(struct t_arg));
+  struct t_arg thread_args[4];
 
-  struct ppm_pixel* image = malloc(sizeof(struct ppm_pixel)*size*size);
-  int* member = malloc(sizeof(int)*size*size);
-  int* counts = malloc(sizeof(int)*size*size);
+  struct ppm_pixel*image = (struct ppm_pixel *) malloc(size*size*sizeof(struct ppm_pixel));
+  bool* member = malloc(sizeof(bool)*size*size);
+  int* count = malloc(sizeof(int)*size*size);
+  memset(count, 0, size * size * sizeof(int));
 
   struct ppm_pixel* palette;
   palette = malloc(sizeof(struct ppm_pixel)*maxIterations);
@@ -180,82 +171,72 @@ int main(int argc, char* argv[]) {
     palette[i].blue = rand() % 255;
   }
 
-  for(int i=0; i<numProcesses; i++){
-    thread_args[i].array = &ids[i];
-    thread_args[i].image = image;
-    thread_args[i].palette = palette;
-    thread_args[i].member = member;
-    thread_args[i].counts = counts;
-    thread_args[i].size = size;
-    thread_args[i].xmin = xmin;
-    thread_args[i].xmax = xmax;
-    thread_args[i].ymin = ymin;
-    thread_args[i].ymax = ymax;
-    thread_args[i].maxIterations = maxIterations;
-    if(i < 2){thread_args[i].row = 0;}else{thread_args[i].row=size/2;}
-    if(i % 2 == 0){thread_args[i].col = 0;}else{thread_args[i].col=size/2;}
-    //thread_args[i].row = 0;
-    //thread_args[i].col = 0;
-  }
+  pthread_barrier_init(&barrier,NULL,4);
+  pthread_mutex_init(&mutex,NULL);
 
-  pthread_mutex_init(&mutex, NULL);
-  for(int i=0; i<numProcesses; i++){
-    ids[i] = i;
-    pthread_create(&threads[i], NULL, detMembership, &thread_args[i]);
-    pthread_create(&threads[i], NULL, compVisits, &thread_args[i]);
-    pthread_create(&threads[i], NULL, compColors, &thread_args[i]);
-  }
-
-  for (int i = 0; i < numProcesses; i++) {
-    pthread_join(threads[i], NULL);
-  }
-  pthread_mutex_destroy(&mutex);
-
-  gettimeofday(&tend, NULL);
-  float timer = tend.tv_sec - tstart.tv_sec + (tend.tv_usec - tstart.tv_usec)/1.e6;
-  printf("Computed buddhabrot set (%d x %d) in %f seconds\n",size, size, timer);
-  char filename[100];
-  sprintf(filename,"buddhabrot-%d-%ld.ppm",size,time(0));
-  printf("writing file: %s\n",filename);
-  write_ppm(filename, image, size, size);
-
-  return 0;
-}
-
-
-struct ppm_pixel* make_one(struct ppm_pixel* pxls, struct ppm_pixel* palette, int size, int row, int col, float xmin, float xmax, float ymin, float ymax, int maxIterations){
-  //creating color array
-  struct ppm_pixel black;
-  black.red = 0;
-  black.green = 0;
-  black.blue = 0;
-
-  for(int i=0; i<size/2; i++){
-    for(int j=0; j<size/2; j++){
-      float xfrac =  (float)(i+row)/(float) size;
-      float yfrac = (float)(j+col)/ (float) size;
-      float x0 = xmin + xfrac*(xmax-xmin);
-      float y0 = ymin + yfrac*(ymax-ymin);
-
-      float x=0;
-      float y=0;
-      int iter = 0;
-      while(iter < maxIterations && x*x + y*y < 2*2){
-        float xtmp = x*x - y*y + x0;
-        y = 2*x*y + y0;
-        x = xtmp;
-        iter ++;
+    for (int i = 0; i < numProcesses; i++) {
+        thread_args[i].image = image;
+        thread_args[i].palette = palette;
+        thread_args[i].xmin = xmin;
+        thread_args[i].xmax = xmax;
+        thread_args[i].ymin = ymin;
+        thread_args[i].ymax = ymax;
+        thread_args[i].maxIterations = maxIterations;
+        thread_args[i].size = size;
+        thread_args[i].member = member;
+        thread_args[i].count = count;
+        if (i == 0){
+            thread_args[i].rStart = 0;
+            thread_args[i].rEnd = size/2;
+            thread_args[i].cStart = 0;
+            thread_args[i].cEnd = size/2;
+        } else if (i == 1){
+            thread_args[i].rStart = 0;
+            thread_args[i].rEnd = size/2;
+            thread_args[i].cStart = size/2;
+            thread_args[i].cEnd = size;
+        } else if (i == 2){
+            thread_args[i].rStart = size/2;
+            thread_args[i].rEnd = size;
+            thread_args[i].cStart = 0;
+            thread_args[i].cEnd = size/2;
+        } else {
+            thread_args[i].rStart = size/2;
+            thread_args[i].rEnd = size;
+            thread_args[i].cStart = size/2;
+            thread_args[i].cEnd = size;
+        }
       }
-      if (iter < maxIterations){
-        pxls[(i+row)+(j+col)*size].red = palette[iter].red;
-        pxls[(i+row)+(j+col)*size].blue = palette[iter].blue;
-        pxls[(i+row)+(j+col)*size].green = palette[iter].green;
-      } else {
-        pxls[(i+row)+(j+col)*size].red = black.red;
-        pxls[(i+row)+(j+col)*size].green = black.green;
-        pxls[(i+row)+(j+col)*size].blue = black.blue;
-      }
+    for(int i = 0; i < numProcesses; i++){
+      pthread_create(&threads[i], NULL, startRoutine, (void*) &thread_args[i]);
+      printf("Thread %d) sub-image block: cols (%d, %d) to rows (%d, %d)\n",
+             (int) threads[i], thread_args[i].cStart, thread_args[i].cEnd, thread_args[i].rStart, thread_args[i].rEnd);
     }
-  }
-  return pxls;
+
+    for (int i = 0; i < 4; i++) {
+        pthread_join(threads[i], NULL);
+        printf("Thread %d) finished\n", (int) threads[i]);
+    }
+
+    pthread_mutex_destroy(&mutex);
+    pthread_barrier_destroy(&barrier);
+
+    gettimeofday(&tend, NULL);
+    timer = tend.tv_sec - tstart.tv_sec + (tend.tv_usec - tstart.tv_usec)/1.e6;
+    printf("Computed buddhabrot set (%dx%d) in %g seconds\n", size, size, timer);
+
+    char filename[100];
+    sprintf(filename, "buddhabrot-%d-%ld.ppm", size, time(0));
+
+    write_ppm(filename, image, size, size);
+    printf("\nWriting file %s\n", filename);
+
+    free(image);
+    image = NULL;
+    free(palette);
+    palette = NULL;
+    free(count);
+    count = NULL;
+    free(member);
+    member = NULL;
 }
