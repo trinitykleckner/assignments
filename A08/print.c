9c3fa875c7c579aaf589101ed727ff9a343105e1
Trinity Rae Kleckner <tkleckner@goldengate.cs.brynmawr.edu>
2022-05-04 13:55:30 -0400

    submitting resubmission

--- A08/print.c
+++ A08/print.c
@@ -1,16 +1,15 @@
-#include <stdlib.h>
+//#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <time.h>
-#include "rand.h"
+//#include <time.h>
 
 int main (int argc, char* argv[]) {
   void* initial = sbrk(0);
   printf("The initial top of the heap is %p\n",initial);
   void* current = sbrk(0);
   printf("The current top of the heap is %p\n",current);
-  unsigned int difference = current - initial;
-  printf("Increased by %d (%#x) bytes\n", difference, difference);
+
+  printf("Increased by %ld (%#lx) bytes\n", current - initial, current - initial);
 
   return 0;
 }
